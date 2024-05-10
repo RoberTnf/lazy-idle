096bbac3322ac547e30674707b3d50a2029a9892
Roberto Diaz <robertodiaztnf@gmail.com>
2024-05-10 15:45:42 +0200

    Revert commented out formulas in Config.tsx

--- src/app/utils/Config.tsx
+++ src/app/utils/Config.tsx
@@ -32,14 +32,14 @@ const startingState = {
     busters: 0,
     setBusters: null,
     time: 0,
-    // workerFormula: "(money.value > worker.cost_next_once)",
-    // factoryFormula: "",
-    // busterFormula: "",
+    workerFormula: "(money.value > worker.cost_next_once)",
+    factoryFormula: "",
+    busterFormula: "",
     unlockedFactories: false,
     unlockedBusters: false,
-    workerFormula: "(money.value > worker.cost_next_once) && ((worker.cost_next_recurrent_all - worker.cost_recurrent_all) * buster.worker_rec_cost_factor < worker.revenue*factory.worker_revenue_factor)",
-    factoryFormula: "(money.value > factory.cost_next_once) && (((factory.worker_revenue_factor_next - factory.worker_revenue_factor) * worker.revenue * worker.count) > (factory.cost_next_recurrent_all - factory.cost_recurrent_all))",
-    busterFormula: "(money.value > buster.cost_next_once) && (((buster.worker_rec_cost_factor - buster.worker_rec_cost_factor_next) * worker.cost_recurrent_all) > (buster.cost_next_recurrent_all - buster.cost_recurrent_all))",
+    // workerFormula: "(money.value > worker.cost_next_once) && ((worker.cost_next_recurrent_all - worker.cost_recurrent_all) * buster.worker_rec_cost_factor < worker.revenue*factory.worker_revenue_factor)",
+    // factoryFormula: "(money.value > factory.cost_next_once) && (((factory.worker_revenue_factor_next - factory.worker_revenue_factor) * worker.revenue * worker.count) > (factory.cost_next_recurrent_all - factory.cost_recurrent_all))",
+    // busterFormula: "(money.value > buster.cost_next_once) && (((buster.worker_rec_cost_factor - buster.worker_rec_cost_factor_next) * worker.cost_recurrent_all) > (buster.cost_next_recurrent_all - buster.cost_recurrent_all))",
 }
 
 export { busterConfig, factoryConfig, startingState, workerConfig }
